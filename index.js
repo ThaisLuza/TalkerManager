7d55f3f7384b55da73618f90c13101e0d1bfa594
Thais Luza <thaisluza21@hotmail.com>
2022-04-25 18:10:09 -0300

    erros lint kkkkk

--- index.js
+++ index.js
@@ -13,32 +13,34 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req,res) => {
+app.get('/talker', async (_req, res) => {
   try {
     const manager = await utilFunc.getManager();
-    if(!manager) {
+    if (!manager) {
       return res.status(200).json([]);
     }
     return res.status(200).json(manager);
   } catch (error) {
     return res.status(500).end();
-}
+  }
 });
 
 app.get('/talker/:id', async (req, res) => {
   try {
     const manager = await utilFunc.getManager();
-    console.log(manager)
-    const searchManager = manager.find(({id}) => id === req.params.id);
+    console.log(manager);
+    const searchManager = manager.find(({ id }) => id === req.params.id);
 
     if (!searchManager) {
-      return res.status(404).json({message: "Pessoa palestrante não encontrada"});
+      return res
+        .status(404)
+        .json({ message: 'Pessoa palestrante não encontrada' });
     }
     return res.status(200).json(searchManager);
   } catch (error) {
     return res.status(500).end();
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log('Online');
